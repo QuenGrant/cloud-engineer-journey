cf57ae12b5a52014aa774bb9f41cfe8265fb8072
Qgrant1 <265623859+QuenGrant@users.noreply.github.com>
2026-09-08 13:28:25 -0400

    Complete AWS resource automation

--- Projects/Cloud-Operations-Dashboard/automation/aws_automation.py
+++ Projects/Cloud-Operations-Dashboard/automation/aws_automation.py
@@ -1,9 +1,10 @@
 import boto3
 
 ec2 = boto3.client("ec2")
+instance_id = "i-01c845150b162d884"
 
 ec2.stop_instances(
-    InstanceIds=["i-01c845150b162d884"]
+    InstanceIds=[instance_id]
 )
 
 print("EC2 stop request sent.")
